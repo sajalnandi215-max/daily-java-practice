3df1a8bcded1b44370e9e33b46de39bf3ea707b5
sajalnandi215-max <sajalnandi215@gmail.com>
2026-02-03 10:35:32 +0530

    DSA(Java): ReverseArray, MaxElement | 2026-02-03

---
+++ Day-2026-02-03/MaxElement.java
@@ -0,0 +1,10 @@
+public class MaxElement {
+    public static void main(String[] args){
+        int[] arr = {10, 45, 23, 67, 89};
+        int max = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max) max = arr[i];
+        }
+        System.out.println("Max: "+max);
+    }
+}
