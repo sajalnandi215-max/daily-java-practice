908be567d92a0aea9554f2760eb050b8c9f021a3
Sajal Nandi <sajalnandi215@gmail.com>
2026-02-07 23:03:39 +0530

    DSA(Java): InsertionSort, LinearSearch | 2026-02-07

---
+++ Day-2026-02-07/LinearSearch.java
@@ -0,0 +1,1 @@
+public class LinearSearch { public static void main(String[] args){ int[] arr={1,2,3,4,5}; int key=3; for(int i=0;i<arr.length;i++){ if(arr[i]==key){ System.out.println("Found"); break;}}}}
