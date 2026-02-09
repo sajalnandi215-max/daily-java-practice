56a68411d0bf68da065a68ec0e9de31c07515b3c
Sajal Nandi <sajalnandi215@gmail.com>
2026-02-10 00:18:41 +0530

    DSA(Java): MaxProductSubarray, MoveZeroes | 2026-02-10

---
+++ Day-2026-02-10/MoveZeroes.java
@@ -0,0 +1,1 @@
+public class MoveZeroes { public static void main(String[] args){ int[] arr={0,1,0,3,12}; int j=0; for(int i=0;i<arr.length;i++){ if(arr[i]!=0) arr[j++]=arr[i]; } while(j<arr.length) arr[j++]=0; System.out.println(java.util.Arrays.toString(arr)); }}
