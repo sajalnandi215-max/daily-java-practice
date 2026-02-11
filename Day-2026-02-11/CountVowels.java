7b472d23674d2385feaae4c566af770551721e7e
Sajal Nandi <sajalnandi215@gmail.com>
2026-02-11 09:36:08 +0530

    DSA(Java): CountVowels, RemoveDuplicatesString | 2026-02-11

---
+++ Day-2026-02-11/CountVowels.java
@@ -0,0 +1,9 @@
+public class CountVowels {
+ public static void main(String[] args){
+  String s="education";
+  int c=0;
+  for(char ch:s.toCharArray())
+   if("aeiou".indexOf(ch)!=-1) c++;
+  System.out.println(c);
+ }
+}
