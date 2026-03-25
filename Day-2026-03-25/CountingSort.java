a7e1df3dfb1b74f27e183a6c0a404d967d98c696
Sajal Nandi <sajalnandi215@gmail.com>
2026-03-25 10:31:53 +0530

    DSA(Java): CountingSort, RadixSort | 2026-03-25

---
+++ Day-2026-03-25/CountingSort.java
@@ -0,0 +1,1 @@
+public class CountingSort { public static void main(String[] args){ int[] arr={4,2,2,8,3,3,1}; int n=arr.length; int max=arr[0]; for(int i=1;i<n;i++) if(arr[i]>max) max=arr[i]; int[] count=new int[max+1]; for(int i=0;i<n;i++) count[arr[i]]++; int index=0; for(int i=0;i<count.length;i++) while(count[i]-->0) arr[index++]=i; System.out.println(java.util.Arrays.toString(arr)); }}
