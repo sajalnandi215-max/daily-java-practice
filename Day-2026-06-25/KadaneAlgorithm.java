e0c61624470f3358d67e955423f4ed99111711f4
Sajal Nandi <sajalnandi215@gmail.com>
2026-06-25 00:08:33 +0530

    DSA(Java): SubarraySum, KadaneAlgorithm | 2026-06-25

---
+++ Day-2026-06-25/KadaneAlgorithm.java
@@ -0,0 +1,1 @@
+public class KadaneAlgorithm { public static void main(String[] args){ int[] arr={-2,1,-3,4,-1,2,1,-5,4}; int maxSoFar=arr[0],maxEnding=arr[0]; for(int i=1;i<arr.length;i++){ maxEnding=Math.max(arr[i],maxEnding+arr[i]); maxSoFar=Math.max(maxSoFar,maxEnding); } System.out.println(maxSoFar); }}
