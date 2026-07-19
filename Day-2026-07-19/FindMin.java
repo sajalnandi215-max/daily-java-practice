715667ed37fbe79f9d5ba2b6edb049154e9aca63
Sajal Nandi <sajalnandi215@gmail.com>
2026-07-19 19:30:29 +0530

    DSA(Java): ExponentialSearch, FindMax | 2026-07-19

---
+++ Day-2026-07-19/FindMin.java
@@ -0,0 +1,1 @@
+public class FindMin { public static void main(String[] args){ int[] arr={5,4,3,2,1}; int min=arr[0]; for(int i=1;i<arr.length;i++) if(arr[i]<min) min=arr[i]; System.out.println(min); }}
